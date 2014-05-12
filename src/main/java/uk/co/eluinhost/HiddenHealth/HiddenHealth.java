4c76761b66cd82b11c2918cc4ce5498ee8a7b873
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-12 16:32:40 +0100

    update to use maven

--- src/main/java/uk/co/eluinhost/HiddenHealth/HiddenHealth.java
+++ src/main/java/uk/co/eluinhost/HiddenHealth/HiddenHealth.java
@@ -1,4 +1,25 @@
-package uk.co.eluinhost.HiddenHealth;
+/*
+ * HiddenHealth.java
+ *
+ * Copyright (c) 2014 Graham Howden <graham_howden1 at yahoo.co.uk>.
+ *
+ * This file is part of HiddenHealth.
+ *
+ * HiddenHealth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HiddenHealth is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HiddenHealth.  If not, see <http ://www.gnu.org/licenses/>.
+ */
+
+package uk.co.eluinhost.hiddenhealth;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
@@ -11,14 +32,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Project: HiddenHealth
- * Package: uk.co.eluinhost.HiddenHealth
- * Class: HiddenHealth
- * Author: Eluinhost
- *
- * Main entry class for the plugin
- */
 public class HiddenHealth extends JavaPlugin {
 
     //the packet identifier for 0x06 Update Health, also contains hunger + saturation
